b0b280e4d8ecad63ad171bc63d50a5bae2775453
Tiv Barlow <78834066+TivBit@users.noreply.github.com>
2023-02-05 23:16:12 -0500

    Add files via upload

---
+++ barlowNativida_M03 Programming_Assignment_Lists_and_Functions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(Date: 05 Feb 2023)s
+
+@author: %(Tiv Barlow)s
+Python Version 3.11.1
+SDEV 220 Spring 2023
+Assignment: M03 Programming Assignment - Lists and Functions
+"""
+
+
+things = ['mozzarella', 'cinderella', 'salmonella']
+print(things)
+
+for i in range(len(things)):
+    things[i] = things[i].upper()
+print(things[1])
+for i in range(len(things)):
+    things[i] = things[i].upper()
+    print(things[0])
+for i in range(len(things)):
+    things[i] = things[i].upper()
+print(things[2])
+
+
