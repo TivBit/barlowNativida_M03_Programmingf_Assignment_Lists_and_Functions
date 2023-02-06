6d15d27d7d95ca8f4dcb97283ba455ba53a0b935
Tiv Barlow <78834066+TivBit@users.noreply.github.com>
2023-02-06 00:48:38 -0500

    Update barlowNativida_M03 Programming_Assignment_Lists_and_Functions.py
    Added two more programs for the assignment.

--- barlowNativida_M03 Programming_Assignment_Lists_and_Functions.py
+++ barlowNativida_M03 Programming_Assignment_Lists_and_Functions.py
@@ -8,10 +8,12 @@ SDEV 220 Spring 2023
 Assignment: M03 Programming Assignment - Lists and Functions
 """
 
+############################################################################
+#This program is designed to print out a list of things.  Some items on
+#the list are to print out capitalized.
 
 things = ['mozzarella', 'cinderella', 'salmonella']
 print(things)
-
 for i in range(len(things)):
     things[i] = things[i].upper()
 print(things[1])
@@ -22,4 +24,35 @@ for i in range(len(things)):
     things[i] = things[i].upper()
 print(things[2])
 
+print()
 
+############################################################################
+#This program is designed to define a function and then print out the names
+#on a list when the function "good" is called.
+
+names = ['Harry', 'Ron', 'Hermione']
+
+def good():
+    print (names)
+
+good()
+
+print()
+
+############################################################################
+#This program is designed with a generator function that returns the odd numbers
+#from range(10). Use a for loop to find and print the third value returned.
+
+
+def odd_numbers( max=0 ):
+   n = 1
+   while n < max:
+       yield 1 * n
+       n += 2
+
+a = odd_numbers(10)
+
+for i in a:
+   print(i)
+
+############################################################################
